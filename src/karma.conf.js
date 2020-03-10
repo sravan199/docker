44ffc9f847a03917c2310297901ea9d4e7232902
sravan <sravan.guduru@berkadia.com>
2020-03-10 18:31:13 +0530

    added travis file

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,8 +25,19 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--no-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     restartOnFileChange: true
   });
 };
